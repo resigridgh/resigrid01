d9c25171a2a06915fbf69f0aace5939dff07877f
resigridgh <resigrid16@gmail.com>
2025-11-16 18:23:40 -0600

    Update regression model

--- src/mypythonpackage/__init__.py
+++ src/mypythonpackage/__init__.py
@@ -1,5 +1,6 @@
 from .model.regression import LinearRegression
 from .model.regression import run_hydropower
-
+from .regression import CauchyRegression
 __all__ = ["LinearRegression", "run_hydropower"]
+__all__ = ['CauchyRegression']
 __version__ = "0.2.0"

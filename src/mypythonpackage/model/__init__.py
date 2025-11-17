f0de40ac2d393497c2588b942c1433eb890b8be1
resigridgh <resigrid16@gmail.com>
2025-11-16 23:26:34 -0600

    Update

--- src/mypythonpackage/model/__init__.py
+++ src/mypythonpackage/model/__init__.py
@@ -2,7 +2,7 @@ from .regression import LinearRegression
 from .regression import run_hydropower
 from .regression import CauchyRegression
 from .logit import ( LogisticRegressionPyTorch, train_model_sgd, evaluate_model)
-
+from .discrete import diff
 
 __all__ = ["LinearRegression", "run_hydropower"]
 __all__ = ['CauchyRegression']
@@ -11,4 +11,4 @@ __all__ = [
     "train_model_sgd",
     "evaluate_model",
     ]
-
+__all__ = ['diff']

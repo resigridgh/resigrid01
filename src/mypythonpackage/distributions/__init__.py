5e74815c7dd5b4c44c145a2e6da3443b26b462a7
resigridgh <resigrid16@gmail.com>
2025-11-17 01:51:37 -0600

    Updated

---
+++ src/mypythonpackage/distributions/__init__.py
@@ -0,0 +1,15 @@
+from .cvdistributions import (
+    uniform, 
+    exponentialdist, 
+    poissondist,
+    generate_samples,
+    discrete_inverse_transform
+)
+
+__all__ = [
+    'uniform', 
+    'exponentialdist', 
+    'poissondist',
+    'generate_samples',
+    'discrete_inverse_transform'
+]

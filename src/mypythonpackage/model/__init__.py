4f992834845285b400b53671287dc516702d7e6f
resigridgh <resigrid16@gmail.com>
2025-11-16 21:43:01 -0600

    Update

--- src/mypythonpackage/model/__init__.py
+++ src/mypythonpackage/model/__init__.py
@@ -1,6 +1,14 @@
 from .regression import LinearRegression
 from .regression import run_hydropower
 from .regression import CauchyRegression
+from .logit import ( LogisticRegressionPyTorch, train_model_sgd, evaluate_model)
+
 
 __all__ = ["LinearRegression", "run_hydropower"]
 __all__ = ['CauchyRegression']
+__all__ = [
+    "LogisticRegressionPyTorch",
+    "train_model_sgd",
+    "evaluate_model",
+    ]
+

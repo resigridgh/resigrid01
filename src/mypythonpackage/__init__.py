4a94991630b662b33d2750450eecea596f5387cb
resigridgh <resigrid16@gmail.com>
2025-11-17 00:22:03 -0600

    Updated

--- src/mypythonpackage/__init__.py
+++ src/mypythonpackage/__init__.py
@@ -1,6 +1,11 @@
 from .model.regression import LinearRegression
 from .model.regression import run_hydropower
 from .regression import CauchyRegression
+from .matrix import rowswap, rowscale, rowreplacement, rref
+
+
 __all__ = ["LinearRegression", "run_hydropower"]
 __all__ = ['CauchyRegression']
+__all__ = ['rowswap', 'rowscale', 'rowreplacement', 'rref']
+
 __version__ = "0.2.0"

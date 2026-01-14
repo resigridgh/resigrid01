9e5858ac287579f2d70fdca0ed8a2488ebec0940
Md Saiful Islam <resigrid16@gmail.com>
2026-01-14 12:32:35 -0600

    Update __init__.py

--- src/mypythonpackage/__init__.py
+++ src/mypythonpackage/__init__.py
@@ -2,10 +2,14 @@ from .model.regression import LinearRegression
 from .model.regression import run_hydropower
 from .regression import CauchyRegression
 from .matrix import rowswap, rowscale, rowreplacement, rref
+from .two_layer_binary_classification import binary_classification
+
+
 
 
 __all__ = ["LinearRegression", "run_hydropower"]
 __all__ = ['CauchyRegression']
 __all__ = ['rowswap', 'rowscale', 'rowreplacement', 'rref']
+__all__ = ["binary_classification"]
 
 __version__ = "0.2.0"

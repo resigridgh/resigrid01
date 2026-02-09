b394390b21a7e682fb359492b750a3c026270f22
Md Saiful Islam <resigrid16@gmail.com>
2026-02-08 18:33:20 -0600

    Update __init__.py

--- src/mypythonpackage/__init__.py
+++ src/mypythonpackage/__init__.py
@@ -1,17 +1,17 @@
-from .model.regression import LinearRegression
-from .model.regression import run_hydropower
-from .regression import CauchyRegression
-from .matrix import rowswap, rowscale, rowreplacement, rref
-from .two_layer_binary_classification import binary_classification
+#from .model.regression import LinearRegression
+#from .model.regression import run_hydropower
+#from .regression import CauchyRegression
+#from .matrix import rowswap, rowscale, rowreplacement, rref
+#from .two_layer_binary_classification import binary_classification
 from .weight_animation import WeightMatrixAnime, animate_weight_heatmap
 from .largewt_animation import LargeWeightMatrixAnime, animate_large_heatmap
 
+from .deepl import binary_classification
 
 
+#__all__ = ["LinearRegression", "run_hydropower"]
+#__all__ = ['CauchyRegression']
+#__all__ = ['rowswap', 'rowscale', 'rowreplacement', 'rref']
+#__all__ = ["binary_classification"]
 
-__all__ = ["LinearRegression", "run_hydropower"]
-__all__ = ['CauchyRegression']
-__all__ = ['rowswap', 'rowscale', 'rowreplacement', 'rref']
-__all__ = ["binary_classification"]
-
-__version__ = "0.2.0"
+#__version__ = "0.2.0"

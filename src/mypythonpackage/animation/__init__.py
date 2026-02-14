8f004c044c0db0a1b1cec410504c4393d8cf5f5b
Md Saiful Islam <resigrid16@gmail.com>
2026-02-13 21:52:50 -0600

    Update __init__.py

--- src/mypythonpackage/animation/__init__.py
+++ src/mypythonpackage/animation/__init__.py
@@ -1,2 +1,10 @@
-from .weight_animation import WeightMatrixAnime, animate_weight_heatmap
+from .weight_animation import WeightMatrixAnime, animate_weight_heatmap, generate_weight_history
 from .largewt_animation import LargeWeightMatrixAnime, animate_large_heatmap
+
+__all__ = [
+    "WeightMatrixAnime",
+    "animate_weight_heatmap",
+    "generate_weight_history",
+    "LargeWeightMatrixAnime",
+    "animate_large_heatmap",
+]

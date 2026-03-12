e7f2960ba32794b1340ed3c84346a06122ead241
resigridgh <resigrid16@gmail.com>
2026-03-12 12:23:37 -0500

    Updated

--- src/mypythonpackage/__init__.py
+++ src/mypythonpackage/__init__.py
@@ -1,13 +1,5 @@
-from . import animation
 from . import deepl
-from . import distributions
-from . import matrix
-from . import model
 
 __all__ = [
-    "animation",
     "deepl",
-    "distributions",
-    "matrix",
-    "model",
 ]

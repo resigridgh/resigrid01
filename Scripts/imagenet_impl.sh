c8795d8d8cd0fd1cc391ac2a5c6f67a43bf0ed2b
resigridgh <resigrid16@gmail.com>
2026-03-12 23:46:29 -0500

    Updated

--- Scripts/imagenet_impl.sh
+++ Scripts/imagenet_impl.sh
@@ -1,5 +1,10 @@
-nohup python imagenet_impl.py \
---epochs 10000 \
---train_ratio 0.1 \
---val_ratio 0.05 \
-> training.log 2>&1 &
+#!/bin/bash
+
+cd ~/resigrid01 || exit 1
+export PYTHONPATH="$(pwd)/src:$PYTHONPATH"
+
+nohup python Scripts/imagenet_impl.py \
+  --epochs 10000 \
+  --train_ratio 0.1 \
+  --val_ratio 0.05 \
+  > training.log 2>&1 &
